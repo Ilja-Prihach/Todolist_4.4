cc1f8edc19737964eeaaca45115a764f0c041004
Ilja-Prihach <piratehnik9508@gmail.com>
2025-10-01 20:08:03 +0300

    add Routing, create 404, and page login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css"
 import { selectThemeMode } from "@/app/app-slice"
-import { Main } from "@/app/Main"
+// import { Main } from "@/app/Main"
 import { ErrorSnackbar, Header } from "@/common/components"
 import { useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
+import { Routing } from "@/common/routing"
+// import { Route, Routes } from "react-router"
+// import { Login } from "@/features/todolists/ui/Login/Login.tsx"
+
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
@@ -17,7 +21,7 @@ export const App = () => {
       <div className={"app"}>
         <CssBaseline />
         <Header />
-        <Main />
+        <Routing/>
         <ErrorSnackbar />
       </div>
     </ThemeProvider>
---
+++ src/common/routing/Routing.tsx
@@ -0,0 +1,21 @@
+import { Main } from "@/app/Main.tsx"
+import { Route, Routes } from "react-router"
+import { Login } from "@/features/todolists/ui/Login/Login.tsx"
+import { PageNotFound } from "@/common/components/PageNotFound/PageNotFound.tsx"
+
+
+export const Path = {
+  Main: '/',
+  Login: 'login',
+  NotFound: '*',
+} as const
+
+export const Routing = () => {
+  return (
+    <Routes>
+      <Route path={Path.Main} element={<Main/>} />
+      <Route path={Path.Login} element={<Login/>} />
+      <Route path={Path.NotFound} element={<PageNotFound/>} />
+    </Routes>
+    )
+}
